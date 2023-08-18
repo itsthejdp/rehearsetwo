8bf7e62497fe69a8ce0ff5490d34354564e7373e
Java <joegross116@gmail.com>
2023-08-18 09:59:52 +0800

    add form and complete nav menu items

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,6 @@ import React from 'react'
 export default function About() {
   return (
     <div>
-        <h1>
-            About Rehearse
-        </h1>
     </div>
-  )
+  );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 
+const manilaCities = ["Bulacan", "Caloocan", "Navotas", "Pasig", "Parañaque", "Quezon City", "San Juan", "Taguig"]
+
+
+
 export default function Form() {
+  const cityOption = manilaCities.map((city, index) => <option key={index} value={city}> {city} </option>)
   return (
     <div>
-        <form action="">
-            <label htmlFor="">Studio Name</label>
-            <input type="text" name="Hey" id="" />
-        </form>
+      <form className="bg-sky-500 flex flex-col p-8 m-12 text-center rounded-2xl">
+        <label htmlFor="">Studio Name</label>
+        <input type="text" name="Hey" id="" />
+        <label htmlFor="">Address </label>
+        <input type="text" name="Hey" id="" />
+        <label htmlFor=""> Studio Location </label>
+        <select>
+          {cityOption}
+        </select>
+        <label htmlFor=""> Studio Image </label>
+        <span className="m-1"><input type="file" name="Hey" id="" /></span>
+        <button className="rounded-full bg-sky-50 p-2 m-1">Add Studio</button>
+      </form>
     </div>
-  )
+  );
 }
+
