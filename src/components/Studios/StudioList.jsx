b7575731bd03b8db46fd10796982822593096865
Java <joegross116@gmail.com>
2023-08-22 08:06:10 +0800

    incorporate state into work

--- src/components/Studios/StudioList.jsx
+++ src/components/Studios/StudioList.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Card from '../Card';
 import Studio from './Studio';
+import { useState } from 'react';
 
 const studios = [
     "Bogchi Studios",
@@ -13,21 +14,27 @@ const studios = [
   const studyas = [{
     id: 0,
     name: "Bogchi Studios",
-    city: "Quezon City"
+    city: "Quezon City",
+    imgURL: "https://www.ipr.edu/wp-content/uploads/2020/02/music-studio-768x432.jpg",
+    description: "A very rugged, yet iconic studio among UP Diliman students located in the heart of its campus."
   }, 
   {
     id: 1,
     name: "Meadowlark Productions",
-    city: "Caloocan City"
+    city: "Caloocan City",
+    imgURL: "https://www.ipr.edu/wp-content/uploads/2020/02/music-studio-768x432.jpg",
+    description: "An amazing, innovative studio, run by a young professional autodidact."
   },
   {
     id: 2,
     name: "Soundkitchen Studios",
-    city: "Quezon City"
+    city: "Quezon City",
+    imgURL: "https://www.ipr.edu/wp-content/uploads/2020/02/music-studio-768x432.jpg",
+    description: "A classic among Maginhawa residents, this one is attached to a famous college hangout spot."
   }]
   
   export default function StudioList({name, children}) {
-    const studioList = studyas.map(studio => <Studio name={studio.name} key={studio.id}>{studio}</Studio>);
+    const studioList = studyas.map(studio => <Studio name={studio.name} imgURL={studio.imgURL} description={studio.description} key={studio.id}>{studio}</Studio>);
     return (
       <div>
         <Card>
